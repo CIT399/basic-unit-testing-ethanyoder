57718b645e7698d3d90d60550bd99520b5f42c61
Ethan Yoder <EMY3@pct.edu>
2018-02-08 15:02:04 -0500

    Switched to use assertTrue and assertFalse rather than assertEquals

--- TelephoneTest.java
+++ TelephoneTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class TestJunit {
 
@@ -11,16 +12,13 @@ public class TestJunit {
    private final String testInput1 = "7174443344";
    private final String testInput2 = "(717)4443344";
    private final String testInput3 = "(717)444-3344";
-   private final boolean testOutput1 = false;
-   private final boolean testOutput2 = false;
-   private final boolean testOutput3 = true;
   
    //this function has 3 test cases for the isFormatted method
    @Test
    public void testIsFormatted() {
-      assertEquals(tele.isFormatted(testInput1), testOutput1);
-      assertEquals(tele.isFormatted(testInput2), testOutput2);
-      assertEquals(tele.isFormatted(testInput3), testOutput3);
+      assertFalse(tele.isFormatted(testInput1));
+      assertFalse(tele.isFormatted(testInput2));
+      assertTrue(tele.isFormatted(testInput3));
    }
  
 }
