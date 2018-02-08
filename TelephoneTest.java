1fe37474f4cad55468a9e823483a3977aca0f1bb
Ethan Yoder <EMY3@pct.edu>
2018-02-08 15:08:53 -0500

    Separated test methods and added setup and tear down

--- TelephoneTest.java
+++ TelephoneTest.java
@@ -5,19 +5,38 @@ import static org.junit.Assert.assertTrue;
 public class TestJunit {
 
    // Creates Telephone object to use for testing
-   Telephone tele = new Telephone();
+   Telephone tele;
 
    // These constant fields hold the input and
    // proper output values for testing isFormatted
    private final String testInput1 = "7174443344";
    private final String testInput2 = "(717)4443344";
    private final String testInput3 = "(717)444-3344";
+   
+   // These functions are used to set up and tear down for the test methods
+   @Before
+   public void setUp() {
+      tele = new Telephone();
+   }
+   
+   @After
+   public void tearDown() {
+      tele = null;  
+   }
   
-   //this function has 3 test cases for the isFormatted method
+   // These functions have 3 test cases for the isFormatted method
    @Test
-   public void testIsFormatted() {
+   public void testIsFormatted1() {
       assertFalse(tele.isFormatted(testInput1));
+   }
+   
+   @Test
+   public void testIsFormatted2() {
       assertFalse(tele.isFormatted(testInput2));
+   }
+   
+   @Test
+   public void testIsFormatted3() {
       assertTrue(tele.isFormatted(testInput3));
    }
  
